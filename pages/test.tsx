8c58b28996aa6fa1c8a5a8fafc5cd843261a3e94
Elijah <111552601+eligibilityy@users.noreply.github.com>
2023-01-19 17:17:02 +0800

    YASSSS

---
+++ pages/test.tsx
@@ -0,0 +1,11 @@
+export default function Info() {
+    return (
+        <>
+            <div className="flex items-center justify-center min-h-screen">
+                <h1 className="text-6xl font-bold">
+                    a
+                </h1>
+            </div>
+        </>
+    )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true
+  },
+  content: [
+    './pages/**/*.{js,jsx,ts,tsx}',
+    './components/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        /** Import PP Mori into sans */
+        sans: ['PP Mori', 'sans-serif', ...defaultTheme.fontFamily.sans],
+      }
+    },
+  },
+  plugins: []
+}
